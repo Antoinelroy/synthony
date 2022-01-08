63d4abbbd7918315a2b94a96be8618fe25c90dc8
Antoinelroy <antoinelroy@hotmail.comgit config --global user.name Antoinegit commit -m first>
2022-01-08 18:24:51 -0500

    commit on master

--- public/login.js
+++ public/login.js
@@ -1,27 +1,92 @@
-var div = document.getElementById("firebaseui-auth-container");
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+
+var div = document.getElementById("modal-login");
+var loginBtn = document.getElementById("loginBtn");
+var logoutBtn = document.getElementById("logoutBtn");
+var presetContainer = document.getElementById("preset-container");
+var presetList = document.getElementById("preset-list");
+
+auth.onAuthStateChanged(user =>{
+
+  if(user){
+    loginBtn.style.display = "none";
+    logoutBtn.style.display = "block";
+    presetContainer.style.display = "flex";
+    presetContainer.style['flex-direction'] = "column";
+
+    db.collection('synth-presets').get().then(snapshot => {
+      setupPresets(snapshot);
+    });
+
+  } else {
+    loginBtn.style.display = "block";
+    logoutBtn.style.display = "none";
+    presetContainer.style.display = "none";
+  }
+
+})
+
+
+
 
 function login() {
     
     div.style.display = "block";
     window.removeEventListener("keydown", handleKeydown);
 
-    ui.start('#firebaseui-auth-container', {
-        signInOptions: [
-          {
-            provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-            requireDisplayName: false
-          }
-        ],
-        signInSuccessUrl: '/public',
-        // Other config options...
-    });
+    const loginForm = document.getElementById("login-form");
+
+    loginForm.addEventListener("submit", (e) =>{
+      e.preventDefault();
+
+      const email = loginForm['login-email'].value;
+      const password = loginForm['login-password'].value;
+
+      auth.signInWithEmailAndPassword(email,password).then(cred =>{
+        div.style.display = "none";
+        window.addEventListener("keydown", handleKeydown);
+      })
+    })
 }
 
-window.onclick = function(event) {
+function logout(){
+  auth.signOut();
+}
+
+function cancel(){
+  div.style.display = "none";
+  window.addEventListener("keydown", handleKeydown);
+}
+
+window.onmousedown = function(event) {
   if (event.target == div) {
     div.style.display = "none";
-    ui.reset();
     window.addEventListener("keydown", handleKeydown);
   }
+}
+
+const setupPresets = (data) => {
+  console.log(data.docs[0].id);
+  if (data) {
+    let html = '';
+    data.forEach(doc => {
+      var preset = doc.data();
+      console.log(doc.id);
+      var presetId = doc.id;
+      const li = `
+        <li class="preset-container2">
+          <div class="preset">
+            <p>${preset['preset-name']}<p>
+            <button class="btnDelete" onclick="deletePreset(${presetId})"></button> 
+          </div>
+        </li>
+      `;
+      html += li;
+    });
+    presetList.innerHTML = html
+  }
+}
+
+const deletePreset = (preset) => {
+  //  console.log(preset);
 }
---
+++ public/savesound.js
@@ -0,0 +1,49 @@
+var presetDiv = document.getElementById("savePresetForm");
+
+function saveSound(){
+    const user = auth.currentUser;
+    presetDiv.style.display = "block";
+    window.removeEventListener("keydown", handleKeydown);
+
+    const presetForm = document.getElementById("preset-form");
+
+    presetForm.addEventListener("submit", (e) =>{
+
+      e.preventDefault();
+      const presetName = presetForm['preset-name'].value;
+
+      if(user){
+
+        db.collection('synth-presets').add({
+            'user-id': user.uid,
+            'preset-name': presetName,
+            cutoff: cutoff,
+            resonance: resonance,
+            osc1WaveType: osc1WaveType,
+            osc1Mix: osc1Mix,
+            osc1Octave: osc1Octave,
+            osc1Detune: osc1Detune,
+            osc2WaveType: osc2WaveType,
+            osc2Octave: osc2Octave,
+            osc2Mix: osc2Mix,
+            osc2Detune: osc2Detune,
+            attackF: attackF,
+            decayF: decayF,
+            sustainF: sustainF,
+            releaseF: releaseF
+        })
+
+        alert('preset saved successfully!');
+        presetDiv.style.display = "none";
+
+    } else {
+        alert('You have to be logged in to save a preset!')
+    }
+
+
+    })
+
+    
+    
+    
+}
